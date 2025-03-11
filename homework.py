40bb474117b21eda012c72813eeedd70f4837aea
andr435 <andr435@gmail.com>
2025-03-11 02:15:02 +0200

    Homework pages 1-42

---
+++ homework.py
@@ -0,0 +1,32 @@
+# Page 28
+
+my_name = "Andrey"
+my_nickname = "andr435"
+my_pet_name = "Sky"
+
+# Page 30
+
+print(f"My name {my_name} aka {my_nickname} and I have a dog {my_pet_name}")
+
+# Page 39
+
+wife_name = "Frekenbok"
+print(type(wife_name))
+wife_age = 56
+wife_age_str = str(wife_age)
+credit_card = {
+    "num" : "312456989987",
+    "cvv" : "123",
+    "valid": "12/38"
+}
+
+print(f"My wife name is {wife_name} she is {wife_age} years old. She is using my credit card with a number {credit_card['num']}")
+
+# Page 42
+
+wifi = "Pentagon guest"
+comp_hostname = "comp-andr435"
+my_con = wifi+' '+comp_hostname
+print(my_con)
+my_con +=str(46)
+print(my_con)
